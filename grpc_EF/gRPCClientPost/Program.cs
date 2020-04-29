fbf5fb98e4332e9815611e41e52d8fed773e6860
alexandrustf <37770439+alexandrustf@users.noreply.github.com>
2020-04-29 13:46:48 +0300

    Add files via upload

---
+++ grpc_EF/gRPCClientPost/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace gRPCClientPost
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new PostComment.Comment();
+            var reply = client.Id;
+            Console.WriteLine("Id of the new Comment created: " + reply);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
