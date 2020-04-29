37b3eb25007baec4e7babaa57709188f540e7fad
alexandrustf <37770439+alexandrustf@users.noreply.github.com>
2020-04-29 13:47:25 +0300

    Add files via upload

---
+++ grpc_EF/gRPCClientPostComment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrpcGreeter;
+using Grpc.Net.Client;
+
+namespace gRPCClientPostComment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
